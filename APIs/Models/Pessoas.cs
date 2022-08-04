12217670f1c54792867b3ec8df4b76645f943d6e
joemyson <joemyson1402@gmail.com>
2022-08-04 01:08:07 -0300

    validando API

--- APIs/Models/Pessoas.cs
+++ APIs/Models/Pessoas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,11 @@ namespace APIs.Models
 {
     public class Pessoas
     {
-
+        [Required(ErrorMessage ="Campo obrigatorio")]
         public string Nome { get; set; }
+        [Range(1,10,ErrorMessage ="o numero deve conter enter 1 e 10 digitos")]
         public int Telefone { get; set; }
+        [Required(ErrorMessage ="Campo obrigatorio")]
         public string Apelido { get; set; }
     }
 }
