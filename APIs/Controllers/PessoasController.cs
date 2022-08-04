baceddfbba61f059a9f6fe57f2ea123f7ec575ee
joemyson <joemyson1402@gmail.com>
2022-08-04 00:48:39 -0300

    iniciando API pessoas

---
+++ APIs/Controllers/PessoasController.cs
@@ -0,0 +1,24 @@
+﻿using APIs.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIs.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class Pessoascontroller : ControllerBase
+    {
+
+    public static List<Pessoas> pessoas = new List<Pessoas>();
+
+    public void AdicionarPessoas([FromBody]Pessoas pessoa)
+    {
+        pessoas.Add(pessoa);
+        Console.WriteLine(pessoa.Nome);
+
+    }
+    }
+}
---
+++ APIs/Models/Pessoas.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIs.Models
+{
+    public class Pessoas
+    {
+
+        public string Nome { get; set; }
+        public int Telefone { get; set; }
+        public string Apelido { get; set; }
+    }
+}
