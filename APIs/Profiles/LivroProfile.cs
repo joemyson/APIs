cb7958c30c518c89a389be71fa30456beba901a0
joemyson <joemyson1402@gmail.com>
2022-08-06 14:51:26 -0300

    criado LivroProfile

---
+++ APIs/Profiles/LivroProfile.cs
@@ -0,0 +1,20 @@
+﻿using APIs.Data.DTO;
+using APIs.Models;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APIs.Profiles
+{
+    public class LivroProfile : Profile
+    {
+        public LivroProfile()
+        {
+            CreateMap<LivroDTO, Livro>();
+            CreateMap<Livro, ReadLivroDTO>();
+            CreateMap<UpDateLivroDTO, Livro>();
+        }
+    }
+}
